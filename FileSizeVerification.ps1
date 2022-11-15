ce33f1286c692f8cf7d96e71e011f2deb1160504
PatKwiatki <115278103+PatKwiatki@users.noreply.github.com>
2022-11-15 18:40:52 -0500

    Create FileSizeVerification.ps1

---
+++ FileSizeVerification.ps1
@@ -0,0 +1,21 @@
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+
+Function Format-FileSize() {
+Param ([int]$size)
+[string]::Format("{0:0.00} GB", $size / 1GB)
+}
+
+$file = 'C:\FileName.ost'
+$size=Format-FileSize((Get-Item $file).length)
+
+
+$ButtonType = [System.Windows.MessageBoxButton]::OK
+$MessageBoxTitle = "Outlook Test"
+$MessageBoxBody = "The outlook file .ost is $size. If the file reaches max size of 50 GB errors may occur. Please clear your old emails to prevent any errors"
+$MessageIcon = [System.Windows.MessageBoxImage]::Warning
+
+
+
+If ($size -gt '40') {
+[System.Windows.MessageBox]::Show($MessageBoxBody,$MessageBoxTitle,$ButtonType,$MessageIcon)
+}
